7c0fa064a9da262804852f8fb06e01d6a8a72ed8
AEVAN <120723479+AEVANLANG@users.noreply.github.com>
2026-03-01 21:23:09 +0100

    Add files via upload

---
+++ MorePlay/ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("MorePlay")
+                .font(.largeTitle)
+            
+            Text("CarPlay Extension Active")
+        }
+    }
+}
---
+++ MorePlay/MorePlayApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct MorePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
